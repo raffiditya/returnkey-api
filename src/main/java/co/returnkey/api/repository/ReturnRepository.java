f4bf597cf99025cf891012d00219902d714c8e26
Raffi Ditya Sukarna <raffi.sukarna@kuelap.io>
2022-06-27 09:30:09 +0700

    remove unused

--- src/main/java/co/returnkey/api/repository/ReturnRepository.java
+++ src/main/java/co/returnkey/api/repository/ReturnRepository.java
@@ -3,13 +3,11 @@ package co.returnkey.api.repository;
 import co.returnkey.api.entity.OrderEntity;
 import co.returnkey.api.entity.ReturnEntity;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReturnRepository extends JpaRepository<ReturnEntity, Long> {
 
-  Optional<ReturnEntity> findByIdAndReturnItems_StatusNot(Long id, String status);
   List<ReturnEntity> findByOrder(OrderEntity order);
 }
